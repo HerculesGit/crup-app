9a162898f188c1042ffa30b3b557f1f2b2bfd428
Hercules Silva de Souza <hercules.silva@dcx.ufpb.br>
2019-11-11 20:53:15 -0300

    contacts.controller.ts fazendo findAll()

--- src/contacts/contacts/contacts.controller.ts
+++ src/contacts/contacts/contacts.controller.ts
@@ -1,13 +1,21 @@
 import { Controller, Get } from '@nestjs/common';
 
+// class
+import { Contact } from '../contact.entity';
+// service
+import { ContactsService } from './contacts.service';
+
 @Controller('contacts')
 export class ContactsController {
 
-    // Todas as solicitações Get vao para cá
-    // Seria como um método getAll
+    // adicionando o contactsService para aí assim usar o CRUD
+    constructor(private contactsService: ContactsService) { }
+
+
+    // Todas as solicitações http://localhost:3000/contacts vão para esse método
     @Get()
-    index(): string {
-        return "This actions will return contacts";
+    index(): Promise<Contact[]> {
+        return this.contactsService.findAll();
     }
 
 }
