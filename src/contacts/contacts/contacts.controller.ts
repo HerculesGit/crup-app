e376c1e08d61b95cb6116c1f131bbdd09d96eaf6
Hercules Silva de Souza <hercules.silva@dcx.ufpb.br>
2019-11-11 21:03:46 -0300

    implemented POST and Put

--- src/contacts/contacts/contacts.controller.ts
+++ src/contacts/contacts/contacts.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body, Put, Param } from '@nestjs/common';
 
 // class
 import { Contact } from '../contact.entity';
@@ -18,4 +18,20 @@ export class ContactsController {
         return this.contactsService.findAll();
     }
 
+    // com esse decoration podemos aceitar método POST pelo endpoint /contacts/create, mandando como BODY os dados do contato
+    // O BODY extrai e injeta o corpo da solicitação POST os dados necessário para nosso método POST
+    @Post('create') // contacts/create
+    async create(@Body() contactData: Contact): Promise<any> {
+        return this.contactsService.create(contactData);
+    }
+
+    // Com isso no endpoint :id/update fazemos a atualização
+    // Algo muito interessante é que nosso id é dinamico, por isso usamos Number(id)
+    // Com o decorator Param('id') extraimos e injetamos o parâmetro para o update
+    @Put(':id/update')
+    async update(@Param('id') id, @Body() contactData: Contact): Promise<any> {
+        contactData.id = Number(id); // pegando o id e transformando em Number
+        console.log(`Update # ${contactData.id}`);
+        return this.contactsService.update(contactData);
+    }
 }
